9c8a6e9f78e1b4a75d8b06230fb0e2dff03b974e
Maida Syed <112477736+MaidaSyed@users.noreply.github.com>
2023-05-06 12:26:41 +0500

    Add files via upload

---
+++ Chapter 2/assets/JS/app.js
@@ -0,0 +1,59 @@
+//Question no 1
+
+// var username;
+ 
+//Q-2
+
+// var myName = "Maida Syed";
+
+//Q-3
+
+// var message = "Hello World";
+// alert(message);
+
+
+//Q-4
+
+// var studentName = "Johne Doe";
+// alert(studentName)
+
+// var age = "15 years old";
+// alert(age);
+
+// var education = "Certified Mobile Application Development";
+// alert(education);
+
+
+
+//Q-5
+// var myFood="PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(myFood);
+
+
+//Q-6
+// var email="maidagogo@gmail.com";
+// alert("My email address is " + email);
+
+
+
+//Q-7
+// var book = " A smarter way to learn JavaScript ";
+// alert("I am trying to learn from the Book" + book);
+
+
+
+//Q-8
+// var content ="Yah! I can write HTML Content through JavaScript";
+// document.write(content);
+
+
+
+//Q-9
+// var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(end);
+
+
+
+
+
+
