7cde0dcf1ced4e65da5266e0d4eaec99ad2ea344
Maida Syed <112477736+MaidaSyed@users.noreply.github.com>
2023-05-14 22:31:36 -0700

    Add files via upload

---
+++ chap-5/app.js
@@ -0,0 +1,136 @@
+// ================ QNO:1 ======================
+// var num1 = 7;
+// var num2 = 4;
+// var add = num1+num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + add);
+
+
+// ================ QNO:2 ======================
+// // FOR SUBTRACTION
+// var num1 = 7;
+// var num2 = 4;
+// var add = num1-num2;
+// document.write("difference of " + num1 + " and " + num2 + " is " + add + "<br>");
+// //FOR MULTIPLICATION
+// var num1 = 7;
+// var num2 = 4;
+// var add = num1*num2;
+// document.write("product of " + num1 + " and " + num2 + " is " + add + "<br>");
+// // FOR DIVISION
+// var num1 = 7;
+// var num2 = 4;
+// var add = num1/num2;
+// document.write("division of " + num1 + " and " + num2 + " is " + add + "<br>");
+// //FOR MODULE
+// var num1 = 7;
+// var num2 = 4;
+// var add = num1%num2;
+// document.write("modulus of " + num1 + " and " + num2 + " is " + add + "<br>");
+
+
+// ================ QNO:3 ======================
+// var empty =
+// document.write("Value after variable declaration: " + empty + "<br>" );
+// empty = 44;
+// document.write("Initian value: " + empty + "<br>");
+// empty = ++empty;
+// document.write("Value after increment is: " + empty + "<br>");
+// empty = empty+7;
+// document.write("Value after addition is: " + empty + "<br>");
+// empty = --empty;
+// document.write("Value after decrement is: " + empty + "<br>");
+// empty = empty%5
+// document.write("The reminder is: " + empty + "<br>");
+
+
+// ================ QNO:4 ======================
+// var oneTicket = 400;
+// var fiveTicket = 5*oneTicket;
+// document.write("Total cost to buy 5 tickets to a movie is " + fiveTicket + "PKR");
+
+
+// ================ QNO:5 ======================
+// document.write("<b>TABLE OF 4</b>" + "<br>");
+// var table = 4;
+// document.write("4 x 1 = " + table*1 + "<br>");
+// document.write("4 x 2 = " + table*2 + "<br>");
+// document.write("4 x 3 = " + table*3 + "<br>");
+// document.write("4 x 4 = " + table*4 + "<br>");
+// document.write("4 x 5 = " + table*5 + "<br>");
+// document.write("4 x 6 = " + table*6 + "<br>");
+// document.write("4 x 7 = " + table*7 + "<br>");
+// document.write("4 x 8 = " + table*8 + "<br>");
+// document.write("4 x 9 = " + table*9 + "<br>");
+// document.write("4 x 10 = " + table*10 + "<br>");
+
+
+// ================ QNO:6 ======================
+// var temInCelsius = 55;
+// var chngInFah = (temInCelsius*9/5)+32
+// var temInFah = 33;
+// var chngInCel = (temInFah - 32)*5/9
+// document.write(temInFah + "*C is " + chngInCel + "*F" + "<br>" );
+// document.write(temInCelsius + "*C is " + chngInFah + "*F" );
+
+
+// ================ QNO:7 ======================
+// var item1 = 400;
+// var quantity1 = 2;
+// var item2 = 100;
+// var quantity2 = 5;
+// var charges = 50;
+// var total = item1*2+item2*5+charges;
+// document.write("Price ot item 1 is " + item1 + "<br>");
+// document.write("Quantity of item 1 is " + quantity1 + "<br>");
+// document.write("Price ot item 2 is " + item2 + "<br>");
+// document.write("Quantity of item 2 is " + quantity2 + "<br>");
+// document.write("Delivery charges is " + charges + "<br>" + "<br>");
+// document.write("Total cost of your order is " + total);
+
+
+// ================ QNO:8 ======================
+// var total = 550;
+// var obtained = 494;
+// var percent = obtained/total*100;
+// document.write("Total marks: " + total + "<br>");
+// document.write("Obtained marks: " + obtained + "<br>");
+// document.write("Percentage: " + Math.round(percent) + "<br>");
+
+
+// ================ QNO:9 ======================
+// document.write("Total Currency In PKR: " + (10*104.8+25*28));
+
+
+// ================ QNO:10 ======================
+// var num = 7;
+// document.write(((num+5)*10)/2);
+
+
+// ================ QNO:11 ======================
+// var currentYear = 2023;
+// var birthYear = 2005;
+// var age = currentYear - birthYear;
+// document.write(age);
+
+
+// ================ QNO:12 ======================
+// var radius = 20;
+// var circumference = 2*3.142*20;
+// var area = 3.14*20*20;
+// document.write("Radius of a circle is: " + radius + "<br>");
+// document.write("The circumference is: " + circumference + "<br>");
+// document.write("The area is: " + area + "<br>");
+
+
+// ================ QNO:13 ======================
+var favSnack = "Lays";
+var age = 17;
+var maxAge = 25;
+var perDay = 2;
+var total = 20*365*2 ;
+document.write("Favourite Snack: " + favSnack + "<br>");
+document.write("Current age: " + age + "<br>");
+document.write("Estimated maximum age: " + maxAge + "<br>");
+document.write("Amount of snacks per day: " + perDay + "<br>");
+document.write("Total Snacks of your life: " + total);
+
