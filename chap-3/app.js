73cf475cc622363606d0c471efa7b101613979c3
Maida Syed <112477736+MaidaSyed@users.noreply.github.com>
2023-05-14 22:30:44 -0700

    Add files via upload

---
+++ chap-3/app.js
@@ -0,0 +1,27 @@
+// ============== QNO:1 =================
+var age = "I AM 17 YEARS OLD";
+alert(age);
+// ============== END ==================
+
+
+// ============== QNO:2 =================
+var numberOfVisit = "You Have Visited This Site 10 times";
+alert(numberOfVisit);
+// ============== END ==================
+
+
+// ============== QNO:3 =================
+var birthYear = "My Birth Year Is 2005";
+document.write(birthYear);
+var lineBreak = "<br>";
+document.write(lineBreak);
+document.write("Data Type Of My Declared Variable is " + typeof+(birthYear));
+// ============== END ==================
+
+
+// ============== QNO:4 =================
+var visitorName = "<b> Elon </b>";
+var productTitle = "ordered";
+var quantity = "<b> 2 pair of Shoes </b>";
+document.write(visitorName + " " + productTitle + " " + quantity + " on XYZ store");
+// ============== END ==================
