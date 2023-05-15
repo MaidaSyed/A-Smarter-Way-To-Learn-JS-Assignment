15be8bd6f7edaaa18007b375fc080d59bdb7391f
Maida Syed <112477736+MaidaSyed@users.noreply.github.com>
2023-05-14 22:31:09 -0700

    Add files via upload

---
+++ chap-4/app.js
@@ -0,0 +1,30 @@
+// ===================== QNO:1 ======================
+// var variable1=hello , variable2=10 , variable3=true
+// ===================== END ======================
+
+
+// ===================== QNO:2 ======================
+// legalVariableNames;
+// 1-camelCaseVariables;
+// 2-$special;
+// 3-_name;
+// 4-my44;
+// 5-you55lucky;
+
+// legalVariableNames;
+// 1-camelcasevariables; (without Camel Case)
+// 2-123special; (starting With Number)
+// 3-@name; (special Characters Except $ and _)
+// 4-function; (reserved keywords)
+// 5-my-number; (without Camel Case)
+// ===================== END ======================
+
+
+// ===================== QNO:3 ======================
+document.write("Varible names can only contain ,numbers,$and _.For example:$my_1stVariable <br><br> ");
+document.write("Varible names begin with letter,$and _.For example:$name,_name or name <br><br>");
+document.write("Varible names are case sensative <br><br>");
+document.write("Varible names should not be js keywords <br><br>");
+// ===================== END ======================
+
+
