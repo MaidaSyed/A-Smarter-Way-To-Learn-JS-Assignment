f138405823a15d911e966213805b38de0519c7dd
Maida Syed <112477736+MaidaSyed@users.noreply.github.com>
2023-07-19 23:15:26 +0500

    assignment 43-48

---
+++ chap 43-48/app.js
@@ -0,0 +1,126 @@
+// ================================ QNO: 1 ===============================
+// var box = document.querySelector("#alert");
+
+// box.addEventListener("click", () => {
+//     alert("Hello");
+// })
+// ================================ END ===============================
+
+
+// ================================ QNO: 2 ===============================
+// var img1 = document.querySelector("#img1");
+// var img2 = document.querySelector("#img2");
+// var img3 = document.querySelector("#img3");
+// var img4 = document.querySelector("#img4");
+
+// img1.addEventListener("click", () => {
+//     alert("It's OPPO Phone")
+// })
+// img2.addEventListener("click", () => {
+//     alert("It's SAMSUNG Phone")
+// })
+// img3.addEventListener("click", () => {
+//     alert("It's iPhone")
+// })
+// img4.addEventListener("click", () => {
+//     alert("It's HUAWEI Phone")
+// })
+// ================================ END ===============================
+
+
+// ================================ QNO: 3 ===============================
+// var btn1 = document.querySelector("#button1");
+// var row1 = document.querySelector("#row1")
+// btn1.addEventListener("click", () => {
+//     row1.innerText = " "
+// })
+
+// var btn2 = document.querySelector("#button2");
+// var row2 = document.querySelector("#row2")
+// btn2.addEventListener("click", () => {
+//     row2.innerText = " "
+// })
+
+// var btn3 = document.querySelector("#button3");
+// var row3 = document.querySelector("#row3")
+// btn3.addEventListener("click", () => {
+//     row3.innerText = " "
+// })
+
+// var btn4 = document.querySelector("#button4");
+// var row4 = document.querySelector("#row4")
+// btn4.addEventListener("click", () => {
+//     row4.innerText = " "
+// })
+
+// var btn5 = document.querySelector("#button5");
+// var row5 = document.querySelector("#row5")
+// btn5.addEventListener("click", () => {
+//     row5.innerText = " "
+// })
+
+// var btn6 = document.querySelector("#button6");
+// var row6 = document.querySelector("#row6")
+// btn6.addEventListener("click", () => {
+//     row6.innerText = " "
+// })
+
+// var btn7 = document.querySelector("#button7");
+// var row7 = document.querySelector("#row7")
+// btn7.addEventListener("click", () => {
+//     row7.innerText = " "
+// })
+
+// var btn8 = document.querySelector("#button8");
+// var row8 = document.querySelector("#row8")
+// btn8.addEventListener("click", () => {
+//     row8.innerText = " "
+// })
+
+// var btn9 = document.querySelector("#button9");
+// var row9 = document.querySelector("#row9")
+// btn9.addEventListener("click", () => {
+//     row9.innerText = " "
+// })
+
+// var btn10 = document.querySelector("#button10");
+// var row10 = document.querySelector("#row10")
+// btn10.addEventListener("click", () => {
+//     row10.innerText = " "
+// })
+// ================================ END ===============================
+
+
+// ================================ QNO: 4 ===============================
+// var image = document.querySelector('#myImage');
+
+// var originalSource = image.src;
+
+// image.addEventListener('mouseover', () => {
+//   image.src = 'https://wallpapercave.com/wp/wp7099449.jpg';
+// });
+
+// image.addEventListener('mouseout', () => {
+//   image.src = originalSource;
+// });
+// ================================ END ===============================
+
+
+// ================================ QNO: 5 ===============================
+// var value = document.querySelector(".box");
+// var incBtn = document.querySelector(".inc");
+// var decBtn = document.querySelector(".dec");
+// var innerValue = 0;
+
+// function defaultValue() {
+//     value.textContent = innerValue
+// }
+// incBtn.addEventListener("click", () => {
+//     innerValue++;
+//     defaultValue()
+// })
+// decBtn.addEventListener("click", () => {
+//     innerValue--;
+//     defaultValue()
+// })
+// ================================ END ===============================
